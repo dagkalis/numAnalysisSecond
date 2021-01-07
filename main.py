f9dd69b398800d7d76b790c029fb2213de20a92c
dagkalis <geodagkalis@gmail.com>
2021-01-07 22:00:47 +0200

    started

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+from numpy import *
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from numpy import log as ln
+import math
+
+import matplotlib.pyplot as plt
+import sys
+
+pi = np.pi
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+# accepts a function, the range and then makes a plot using all that
+def plotter(fun, array, x1, x2):
+    x = np.arange(x1, x2, 0.01)
+    # y = zeros(len(x))
+    # plt.plot(x, y)
+    plt.plot(x, fun(x), color="orange")
+
+    plt.scatter(array, fun(array), color="black")
+
+    plt.show()
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # for i in range(10):
+    #     print(round(random.uniform(-pi, pi), 4))
+
+    arrayList = [2.9193,
+-1.9475,
+0.5656,
+-1.379,
+2.1096,
+-0.2275,
+0.0781,
+1.1325,
+2.7807,
+-0.6981,
+]
+
+    plotter(np.sin, arrayList, -pi, pi)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
